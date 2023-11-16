f2c2b2b522bfd4c7fb30c70dccbab6cd71aedc16
Muchiri-cmd <davismuchiri21@gmail.com>
2023-11-16 16:28:35 +0300

    remove array elements

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array,...elems) {
+    const cleanedArray=[];
+    array.forEach((elem)=>{
+        if (!elems.includes(elem)){
+            cleanedArray.push(elem);
+        }
+    });
+    return cleanedArray;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
