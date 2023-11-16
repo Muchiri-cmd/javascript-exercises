10c541bbafd783ad7fb8a272c81a818b89c72be3
Muchiri-cmd <davismuchiri21@gmail.com>
2023-11-16 16:43:04 +0300

    Sum all numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(inta,intb) {
+    let sum=0;
+    for(let i=inta;inta<=intb;i++){
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
