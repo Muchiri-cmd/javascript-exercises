bdd8793f548c5adeecd57e30534b354778a07f0e
Muchiri-cmd <davismuchiri21@gmail.com>
2023-11-16 10:56:42 +0300

    Reverse a string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+//return string.split("").reverse().join("");
 
+const reverseString = function(string) {
+    let reversedString='';
+    let array=string.split("");
+    while (array.length>0){
+        let letter=array.pop();
+        reversedString+=letter;
+    }
+    return reversedString
 };
-
 // Do not edit below this line
 module.exports = reverseString;
