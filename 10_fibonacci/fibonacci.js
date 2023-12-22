4e1d9358cf0a82234fa00a907938f49c909eff48
Muchiri-cmd <davismuchiri21@gmail.com>
2023-12-22 06:14:11 +0300

    Refactor fibonacci.js solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,13 +1,10 @@
 const fibonacci = function(position) {
-    if (position<0){
-        return "OOPS";
-    }else if(typeof(position)=='string'){
-        Number(position);
-    }
+    if (position<0) return "OOPS";
+    if(typeof(position)=='string'){Number(position);}
+
     const fibonacciArray=[1,1];
     for(let i=1;i<position;i++){
-        const nextNum=fibonacciArray[i]+fibonacciArray[i-1]
-        fibonacciArray.push(nextNum);
+        fibonacciArray.push(fibonacciArray[i]+fibonacciArray[i-1]);
     }
     return fibonacciArray[position-1];
 
