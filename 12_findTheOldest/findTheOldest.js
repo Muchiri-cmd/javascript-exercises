a369cdccca0f05615493ea79bd3cfed3f996dc63
Muchiri-cmd <davismuchiri21@gmail.com>
2023-12-22 07:31:15 +0300

    Complete findTheOldest.js solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let greatest=0;
+    const sortedArray=[];
+    for(obj of arr){
+        if (obj.yearOfDeath){
+            const age=obj.yearOfDeath-obj.yearOfBirth;
+            if (age>greatest){
+                greatest=age;
+                sortedArray.push(obj);
+            }
+        }else if (!obj.yearOfDeath){
+            const age= (new Date().getFullYear())-obj.yearOfBirth;
+            if (age>greatest){
+                greatest=age;
+                sortedArray.push(obj)
 
+            }
+        }
+     }
+     //sort array according to age
+     return sortedArray.reverse()[0]
+
+
+    
 };
 
 // Do not edit below this line
