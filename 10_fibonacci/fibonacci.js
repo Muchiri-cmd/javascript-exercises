43cc63aea7bb6a8ee9fdaa3362fa7ddfb844e69a
Muchiri-cmd <davismuchiri21@gmail.com>
2023-12-22 06:06:05 +0300

    Complete fibonacci.js solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
+    if (position<0){
+        return "OOPS";
+    }else if(typeof(position)=='string'){
+        Number(position);
+    }
+    const fibonacciArray=[1,1];
+    for(let i=1;i<position;i++){
+        const nextNum=fibonacciArray[i]+fibonacciArray[i-1]
+        fibonacciArray.push(nextNum);
+    }
+    return fibonacciArray[position-1];
 
 };
 
